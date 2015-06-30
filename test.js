b10a623fa629a73ad0b1c4bd28680dc063f15e57
Greg Wicks <gpwicks@email.wm.edu>
2015-06-30 13:26:01 -0400

    Add shrug

--- test.js
+++ test.js
@@ -47,6 +47,9 @@ io.on('connection', function(socket){
                 else if (message=='/hannah') {
                         io.emit('hannah', {"id":getId(userIds.arr, socket.id).userName}); 
                 }
+                else if (message=='/shrug') {
+                        io.emit('shrug', {"id":getId(userIds.arr, socket.id).userName});
+                }
 		else{
 			io.emit('chat message', {"message":message, "id":getId(userIds.arr, socket.id).userName});
 			chatMessages.push(getId(userIds.arr, socket.id).userName + ": " + message);
